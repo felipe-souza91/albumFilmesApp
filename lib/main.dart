d587705815350d03ee9afaaabe860d87cf9af959
Luis Souza <felipe_souza91@live.com>
2025-04-15 10:45:25 -0300

    Criação de script para carga de dados no banco

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'services/update_movies.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: Text('Test App')),
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(
+            onPressed: () async {
+              final updater = MovieUpdater();
+              await updater
+                  .updateFirestore(8499489); // Substitua pelo seu listId
+            },
+            child: Text('Update Movies'),
+          ),
         ),
       ),
     );
